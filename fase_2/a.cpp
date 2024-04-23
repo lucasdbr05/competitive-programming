35d5e86b10a1b594b1f8354ee0c5169ad9100e39
lucasdbr05 <lucasgabrieljake@gmail.com>
2024-04-23 00:21:00 -0300

    comp

--- fase_2/a.cpp
+++ fase_2/a.cpp
@@ -13,21 +13,60 @@ using namespace std;
 #define sws ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
 const string YES = "YES";
 const string NO = "NO";
-const int MAX= 1e6+5;
+const int MAX= 2e5+5;
 const int MOD= 1e9+7;
 const int INF = 0x3f3f3f3f3f3f3f3f;
+bool vis[MAX];
+vector<int> g[MAX];
+int idx[MAX];
+map<int, int> mp, paths;
+int cnt = 0, adj = 0;
+
+void dfs(int v, int id){
+    if(vis[v]) return;
+
+    vis[v]=true;
+    idx[v]= id;
+    mp[id]++;
+    cnt++;
+    adj+= g[v].size();
+    int aux = 0;
+    for(auto u:g[v]){
+        dfs(u, id);
+    }
+}
+
+
 
 void fluminense(){
     int n, m; cin >> n >> m;
 
-    vector<int> v(n+3);
-    for(int i=0; i<n;i++){
-        cin >> v[i];
+    while(m--){
+        int u, v; cin >> u >> v;
+        g[u].pb(v);
+        g[v].pb(u);
     }
-    vector<int> ans(n+3);
-    for(int i=1; i<=n; i++){
-        ans
+    int ans =0;
+    for(int i=1;i<=n; i++){
+        if(!vis[i]){
+            cnt = 0;
+            adj = 0;
+            dfs(i, i);
+            cout << cnt << " " << adj << endl;
+            ans+= (cnt*(cnt-1))/2 - adj/2;
+        }
     }
+
+    cout << ans << endl;
+
+
+
+
+    
+
+    
+
+
 }
 
 int32_t main(){
